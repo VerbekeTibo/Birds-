191851c6e070c189187b94be57b00f14307497f6
Tibo Verbeke <tibo.verbeke@student.howest.be>
2022-09-21 16:36:08 +0200

    added active classes

--- pwa/src/screens/generic/AppNavigation.vue
+++ pwa/src/screens/generic/AppNavigation.vue
@@ -1,35 +1,35 @@
 <template>
     <div class="flex justify-end w-full gap-12 mr-8 font-bold">
             <p class="hover:text-neutral-600">
-                <router-link to="/">
+                <router-link class=""  to="/">
                     <Home class="mx-auto mb-2 h-5 sm:hidden"/>
                     Home
                 </router-link>
             </p>
             <p class="hover:text-neutral-600">
-                <router-link to="/birds">
+                <router-link active-class="opacity-40" to="/birds">
                     <Feather class="mx-auto mb-2 h-5 sm:hidden"/>
                     Birds
                 </router-link>
             </p>
             <p class="hover:text-neutral-600">
-                <router-link to="/log">
+                <router-link active-class="opacity-40" to="/log">
                     <Scroll class="mx-auto mb-2 h-5 sm:hidden"/>
                     Logs
                 </router-link>
             </p>
             <p class="hover:text-neutral-600">
-                <router-link to="/observations">
+                <router-link active-class="opacity-40" to="/observations">
                     <Clipboard class="mx-auto mb-2 h-5 sm:hidden"/>
                     Observations
                 </router-link>
             </p>
             <p class="hover:text-neutral-600">
-                <router-link v-if="user" to="/account">
+                <router-link active-class="opacity-40" v-if="user" to="/account">
                     <User class="mx-auto mb-2 h-5 sm:hidden"/>
                     {{ user?.displayName }}
                 </router-link>
-                <router-link to="/auth/login" v-else>
+                <router-link active-class="opacity-40" to="/auth/login" v-else>
                     <LogIn class="mx-auto mb-2 h-5 sm:hidden"/>Log in</router-link>
             </p>
         </div>
