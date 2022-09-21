786dbfcf257db4c9a13da6d60c1fc63b64828c17
Martijn Loth <MLoth@users.noreply.github.com>
2022-09-21 16:23:29 +0200

    feat: started navs

--- pwa/src/components/generic/AppFooter.vue
+++ pwa/src/components/generic/AppFooter.vue
@@ -1,5 +1,23 @@
 <template>
-  <footer>
-    <p>Martijn Loth - Disclaimer {{ new Date().getFullYear() }}</p>
-  </footer>
+  <div class="px-6 pt-3 pb-6">
+    <footer class="mx-auto hidden max-w-6xl sm:block">
+      <p class="text-sm text-neutral-800">
+        Martijn Loth - Disclaimer {{ new Date().getFullYear() }}
+      </p>
+    </footer>
+
+    <nav class="block sm:hidden">
+      <AppNavigation />
+    </nav>
+  </div>
 </template>
+
+<script lang="ts">
+import AppNavigation from './AppNavigation.vue'
+
+export default {
+  components: {
+    AppNavigation,
+  },
+}
+</script>
--- pwa/src/components/holders/AppHolder.vue
+++ pwa/src/components/holders/AppHolder.vue
@@ -1,16 +1,20 @@
 <template>
-  <div>
-    <router-view></router-view>
+  <div class="grid h-screen min-h-screen grid-rows-[auto_1fr_auto] sm:h-auto">
+    <AppHeader />
+
+    <router-view class="overflow-y-auto"></router-view>
 
     <AppFooter />
   </div>
 </template>
 
 <script lang="ts">
+import AppHeader from '../generic/AppHeader.vue'
 import AppFooter from '../generic/AppFooter.vue'
 
 export default {
   components: {
+    AppHeader,
     AppFooter,
   },
 }
