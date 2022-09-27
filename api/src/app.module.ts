620d4bb39d7cc9a5bff9a820e31b9e97f29d851b
Tibo Verbeke <tibo.verbeke@student.howest.be>
2022-09-27 12:35:40 +0200

    end lesson

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './bootstrap/typeOrmConfig';
 import { BirdsModule } from './birds/birds.module';
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { graphqlConfig } from './bootstrap/graphqlConfig';
+import { GraphQLModule } from '@nestjs/graphql';
 
 //TODO: bird module
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BirdsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), 
+  GraphQLModule.forRoot<ApolloDriverConfig>(graphqlConfig)],
   controllers: [],
   providers: [],
 })
---
+++ api/src/bootstrap/graphqlConfig.ts
@@ -0,0 +1,9 @@
+import { ApolloDriver } from "@nestjs/apollo";
+import { GqlModuleOptions } from "@nestjs/graphql";
+
+export const graphqlConfig: GqlModuleOptions = {
+    driver: ApolloDriver,
+    sortSchema: true,
+    autoSchemaFile: true,
+
+}
