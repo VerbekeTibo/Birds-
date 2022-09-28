432d48ba0445dc0afca737fd95bc20a877513a86
Martijn Loth <MLoth@users.noreply.github.com>
2022-09-28 14:51:50 +0200

    chore: cleanup dist folder

--- api/dist/birds/dto/update-bird.input.d.ts
+++
@@ -1,6 +0,0 @@
-import { CreateBirdInput } from './create-bird.input';
-declare const UpdateBirdInput_base: import("@nestjs/common").Type<Partial<CreateBirdInput>>;
-export declare class UpdateBirdInput extends UpdateBirdInput_base {
-    id: string;
-}
-export {};
