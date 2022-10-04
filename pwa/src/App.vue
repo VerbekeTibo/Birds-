c7410a9c07ce7e034565b036a5fc6df222b19f2b
Martijn Loth <MLoth@users.noreply.github.com>
2022-10-04 09:49:43 +0200

    feat: start graphql & bird images

--- pwa/src/App.vue
+++ pwa/src/App.vue
@@ -3,18 +3,20 @@
 </template>
 
 <script lang="ts">
+import { provide } from '@vue/runtime-core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
 import useFirebase from './composables/useFirebase'
+import useGraphQL from './composables/useGraphQL'
 
 export default {
   setup() {
     const { app } = useFirebase()
+    const { apolloClient } = useGraphQL()
+
+    provide(DefaultApolloClient, apolloClient)
 
     return {}
   },
 }
 </script>
-
-<style scoped lang="scss">
-.selector {
-}
-</style>
--- pwa/src/screens/birds/index.vue
+++ pwa/src/screens/birds/index.vue
@@ -1,3 +1,33 @@
 <template>
   <h1>Birds</h1>
 </template>
+
+<script lang="ts">
+import gql from 'graphql-tag'
+import { useQuery } from '@vue/apollo-composable'
+
+export default {
+  setup() {
+    const BIRDS = gql`
+      query birds {
+        birds {
+          id
+          name
+          url
+          description
+          category
+        }
+      }
+    `
+
+    const { result, loading, error } = useQuery(BIRDS)
+    console.table(result.value.birds)
+
+    return {
+      result,
+      loading,
+      error,
+    }
+  },
+}
+</script>
